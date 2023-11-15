d3b56754fabb06d20798152fb43f4be59ba80c50
1Max58 <>
2023-11-15 20:43:34 +0200

    Animal fields and abstract methods added

--- src/com/example/animals/Animal.java
+++ src/com/example/animals/Animal.java
@@ -1,4 +1,11 @@
 package com.example.animals;
 
 public abstract class Animal {
+    private String nameKorsun64068;
+    private String ageKorsun64068;
+    private String weightKorsun64068;
+
+    public abstract void eatKorsun64068();
+    public abstract void getVoidKorsun64068();
+
 }
