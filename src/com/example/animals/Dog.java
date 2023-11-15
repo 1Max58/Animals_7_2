bb1bf91e3f05e2c38df64ae898828539d929f48e
1Max58 <>
2023-11-15 21:53:37 +0200

    Mammal and Dog fields and methods added

--- src/com/example/animals/Dog.java
+++ src/com/example/animals/Dog.java
@@ -1,4 +1,36 @@
 package com.example.animals;
 
 public class Dog extends Mammal {
+    private String breedKorsun64068;
+
+    public Dog() {
+        super();
+        breedKorsun64068 = "";
+    }
+
+    public Dog(String n, int a, float w, int mw, String s) {
+        super(n, a, w, mw);
+        breedKorsun64068 = s;
+    }
+
+    public Dog(String n, String s) {
+        super(n, 40);
+        breedKorsun64068 = s;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", it is a Dog of the " + breedKorsun64068 + " breed";
+    }
+
+    public void bark() {
+        System.out.println("Dog " + getNameKorsun64068() + " barks");
+    }
+    public String getBreedKorsun64068() {
+        return breedKorsun64068;
+    }
+
+    public void setBreedKorsun64068(String breedKorsun64068) {
+        this.breedKorsun64068 = breedKorsun64068;
+    }
 }
--- src/com/example/animals/Mammal.java
+++ src/com/example/animals/Mammal.java
@@ -1,4 +1,46 @@
 package com.example.animals;
 
 public class Mammal extends Animal {
+    private int maxWeightKorsun64068;
+
+    public Mammal() {
+        super();
+        maxWeightKorsun64068 = 0;
+    }
+
+    public Mammal(String n, int a, float w, int mw) {
+        super(n, a, w);
+        maxWeightKorsun64068 = mw;
+    }
+
+    public Mammal(String n, int mw) {
+        super(n);
+        maxWeightKorsun64068 = mw;
+    }
+
+    public void run() {
+        System.out.println("Mammal " + getNameKorsun64068() + " runs");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", it is a Mammal up to " + maxWeightKorsun64068 + " kg";
+    }
+    @Override
+    public void eatKorsun64068() {
+        System.out.println("Mammal " + getNameKorsun64068() + " eats");
+    }
+
+    @Override
+    public void getVoiceKorsun64068() {
+        System.out.println("Mammal " + getNameKorsun64068() + " gives voice");
+    }
+
+    public int getMaxWeightKorsun64068() {
+        return maxWeightKorsun64068;
+    }
+
+    public void setMaxWeightKorsun64068(int maxWeightKorsun64068) {
+        this.maxWeightKorsun64068 = maxWeightKorsun64068;
+    }
 }
