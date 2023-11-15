32f6293d14252cee0bd991d00fa527337ba7b275
1Max58 <>
2023-11-15 21:42:14 +0200

    Fish and Blowfish fields and methods added

--- src/com/example/animals/Blowfish.java
+++ src/com/example/animals/Blowfish.java
@@ -1,4 +1,19 @@
 package com.example.animals;
 
 public class Blowfish extends Fish {
+    public String blowfishSpeciesKorsun64068;
+    public Blowfish() {
+        super();
+        blowfishSpeciesKorsun64068 = "";
+    }
+
+    public Blowfish(String n, boolean fw, String s) {
+        super(n, fw);
+        blowfishSpeciesKorsun64068 = s;
+    }
+
+    public Blowfish(String n) {
+        super(n, false);
+        blowfishSpeciesKorsun64068 = "Blowfish spp.";
+    }
 }
--- src/com/example/animals/Fish.java
+++ src/com/example/animals/Fish.java
@@ -1,4 +1,47 @@
 package com.example.animals;
 
 public class Fish extends Animal {
+    private boolean isFreshwaterKorsun64068;
+
+    public Fish() {
+        super();
+        isFreshwaterKorsun64068 = false;
+    }
+
+    public Fish(String n, int a, float w, boolean fw) {
+        super(n, a, w);
+        isFreshwaterKorsun64068 = fw;
+    }
+
+    public Fish(String n, boolean fw) {
+        super(n);
+        isFreshwaterKorsun64068 = fw;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", it is " + (isFreshwaterKorsun64068 ? "freshwarer" : "saltwater") + " Fish";
+    }
+
+    @Override
+    public void eatKorsun64068() {
+        System.out.println("Fish " + getNameKorsun64068() + " eats");
+    }
+
+    @Override
+    public void getVoiceKorsun64068() {
+        System.out.println("Fish " + getNameKorsun64068() + " gives no voice");
+    }
+
+    public void swim() {
+        System.out.println("Fish " + getNameKorsun64068() + " swims");
+    }
+
+    public boolean isFreshwaterKorsun64068() {
+        return isFreshwaterKorsun64068;
+    }
+
+    public void setFreshwaterKorsun64068(boolean freshwater) {
+        isFreshwaterKorsun64068 = freshwater;
+    }
 }
